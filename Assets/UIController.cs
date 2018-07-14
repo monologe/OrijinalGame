f39fb0dbb9f4d5e75d2b934c0d8c56eac414506c
Sumire Kikuta <paddyfield815@gmail.com>
2018-07-14 22:48:11 +0900

    ゲーム前画面OK

--- Assets/UIController.cs
+++ Assets/UIController.cs
@@ -32,6 +32,25 @@ public class UIController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //スタート前
+            if (Music.IsJustChangedAt(0, 0, 0))
+            {
+                this.SongNameText.GetComponent<Text>().text = "Song Name 夏の秘密基地";
+            }
+            if (Music.IsJustChangedAt(1, 3, 3))
+            {
+            Destroy(GameObject.Find("SongName"));
+            }
+            if (Music.IsJustChangedAt(2, 0, 0))
+            {
+                this.AreYouReadyText.GetComponent<Text>().text = "Are you ready?";
+            }
+            if (Music.IsJustChangedAt(4, 0, 0))
+            {
+            Destroy(GameObject.Find("AreYouReady"));
+            }
+
+
         // ゲームオーバーになった場合
         if (this.isGameOver)
         {
